070a069f3bdcf0c151df88309db96aa438e2566e
Jake Hickenlooper <jake@weboftomorrow.com>
2016-08-25 21:40:15 -0600

    Initial ViewFinder

--- src/slab-massive.js
+++ src/slab-massive.js
@@ -2,18 +2,28 @@
 import style from './slab.css'
 import template from './slab-massive.html'
 
-let html = `
+const html = `
   <style>${style}</style>
   ${template}
   `
 class SlabMassive extends HTMLElement {
   // Fires when an instance of the element is created.
   createdCallback () {
+    console.log('created')
     this.createShadowRoot().innerHTML = html
-    this.element = this.shadowRoot.querySelector('.sm-Slab')
+    this.slab = this.shadowRoot.querySelector('.sm-Slab')
     this.container = this.shadowRoot.querySelector('.sm-Slab-container')
     this.slot = this.shadowRoot.querySelector('.sm-Slab-slot')
+    this.viewFinder = this.shadowRoot.querySelector('.sm-ViewFinder')
+    this.viewFinderBox = this.shadowRoot.querySelector('.sm-ViewFinder-box')
+
     this.render()
+    let zoomInEl = this.shadowRoot.querySelector('.sm-Slab-zoomIn')
+    let zoomIn = this.zoomIn.bind(this)
+    zoomInEl.addEventListener('click', zoomIn)
+    let zoomOutEl = this.shadowRoot.querySelector('.sm-Slab-zoomOut')
+    let zoomOut = this.zoomOut.bind(this)
+    zoomOutEl.addEventListener('click', zoomOut)
   }
 
   // Fires when an instance was inserted into the document.
@@ -21,12 +31,36 @@ class SlabMassive extends HTMLElement {
 
   // Fires when an attribute was added, removed, or updated.
   attributeChangedCallback (attrName, oldVal, newVal) {
-    console.log('change', attrName)
+    if (oldVal !== newVal) {
+      console.log('change', attrName)
+      this[attrName] = newVal
+    }
+    switch (attrName) {
+      case 'zoom':
+        this.renderSlotPosition()
+        break
+      case 'offset-x':
+        this.renderSlotPosition()
+        break
+      case 'offset-y':
+        this.renderSlotPosition()
+        break
+      case 'scale':
+        this.render()
+        break
+      case 'width':
+        this.render()
+        break
+      case 'height':
+        this.render()
+        break
+    }
   }
 
   render () {
-    this.element.style.width = (this.width * this.scale) + 'px'
-    this.element.style.height = (this.height * this.scale) + 'px'
+    console.log('render', this.slab)
+    this.slab.style.width = (this.width * this.scale) + 'px'
+    this.slab.style.height = (this.height * this.scale) + 'px'
 
     this.container.style.width = this.width + 'px'
     this.container.style.height = this.height + 'px'
@@ -34,13 +68,32 @@ class SlabMassive extends HTMLElement {
     this.container.style.marginLeft = ((this.width / 2) * -1) + 'px'
     this.container.style.marginTop = ((this.height / 2) * -1) + 'px'
 
+    this.viewFinder.scale = (150 / this.width)
+    this.viewFinder.style.width = '150px'
+    this.viewFinder.style.height = Math.floor(this.viewFinder.scale * this.height) + 'px'
+    this.viewFinderBox.style.height = Math.floor(this.height * this.viewFinder.scale) + 'px'
+    this.viewFinderBox.style.width = Math.floor(this.width * this.viewFinder.scale) + 'px'
+
     this.slot.style.width = this.width + 'px'
     this.slot.style.height = this.height + 'px'
     this.renderSlotPosition()
   }
 
   renderSlotPosition () {
-    this.slot.style.transform = `translate(${this.offsetX}, ${this.offsetY}) scale(${this.zoom})`
+    this.slot.style.transform = `translate(${this.offsetX}px, ${this.offsetY}px) scale(${this.zoom})`
+
+    // Update the viewFinder box position and size
+    console.log(`translate(${Math.floor((this.offsetX / this.zoom) * this.viewFinder.scale)}px, ${Math.floor((this.offsetY / this.zoom) * this.viewFinder.scale)}px)`)
+    //this.viewFinderBox.style.transform = `translate(${Math.floor((this.offsetX / this.zoom) * this.viewFinder.scale)}px, ${Math.floor((this.offsetY / this.zoom) * this.viewFinder.scale)}px)`
+    this.viewFinderBox.style.transform = `scale(${1 / this.zoom}) translate(${Math.floor((this.offsetX / this.zoom) * this.viewFinder.scale)}px, ${Math.floor((this.offsetY / this.zoom) * this.viewFinder.scale)}px)`
+  }
+
+  zoomIn () {
+    this.setAttribute('zoom', this.zoom * 2)
+  }
+
+  zoomOut () {
+    this.setAttribute('zoom', Math.max(this.zoom / 2, 1.0))
   }
 
   // Reflect the width prop with the attr
@@ -49,7 +102,13 @@ class SlabMassive extends HTMLElement {
   }
   set scale (val) {
     this.setAttribute('scale', val)
-    this.render()
+  }
+
+  get zoom () {
+    return this.getAttribute('zoom')
+  }
+  set zoom (val) {
+    this.setAttribute('zoom', val)
   }
 
   get offsetX () {
@@ -57,7 +116,6 @@ class SlabMassive extends HTMLElement {
   }
   set offsetX (val) {
     this.setAttribute('offset-x', val + 'px')
-    this.renderSlotPosition()
   }
 
   get offsetY () {
@@ -65,7 +123,6 @@ class SlabMassive extends HTMLElement {
   }
   set offsetY (val) {
     this.setAttribute('offset-y', val + 'px')
-    this.renderSlotPosition()
   }
 
   get width () {
@@ -73,7 +130,6 @@ class SlabMassive extends HTMLElement {
   }
   set width (val) {
     this.setAttribute('width', val)
-    this.render()
   }
 
   get height () {
@@ -81,7 +137,6 @@ class SlabMassive extends HTMLElement {
   }
   set height (val) {
     this.setAttribute('height', val)
-    this.render()
   }
 }
 
